eb222205fe76faa005d82837a8db4318d3830c42
octaprog7 <goctaprog@gmail.com>
2022-08-22 22:07:43 +0500

    move to new version of sensor_pack

--- sensor_pack/base_sensor.py
+++ sensor_pack/base_sensor.py
@@ -1,12 +1,46 @@
+# micropython
+# MIT license
+# Copyright (c) 2022 Roman Shevchik   goctaprog@gmail.com
+import micropython
+import ustruct
 from sensor_pack import bus_service
 
 
 class BaseSensor:
     """Base sensor class"""
 
-    def __init__(self, adapter: bus_service.BusAdapter, address: int):
+    def __init__(self, adapter: bus_service.BusAdapter, address: int, big_byte_order: bool):
+        """Базовый класс Датчик.
+        Если big_byte_order равен True -> порядок байтов в регистрах датчика «big»
+        (Порядок от старшего к младшему), в противном случае порядок байтов в регистрах "little"
+        (Порядок от младшего к старшему)
+        address - адрес датчика на шине.
+
+        Base sensor class. if big_byte_order is True -> register values byteorder is 'big'
+        else register values byteorder is 'little'
+        address - address of the sensor on the bus."""
         self.adapter = adapter
         self.address = address
+        self.big_byte_order = big_byte_order
+
+    def _get_byteorder_as_str(self) -> tuple:
+        """Return byteorder as string"""
+        if self.is_big_byteorder():
+            return 'big', '>'
+        else:
+            return 'little', '<'
+
+    def unpack(self, fmt_char: str, source: bytes) -> tuple:
+        """распаковка массива, считанного из датчика.
+        fmt_char: c, b, B, h, H, i, I, l, L, q, Q. pls see: https://docs.python.org/3/library/struct.html"""
+        if len(fmt_char) != 1:
+            raise ValueError(f"Invalid length fmt_char parameter: {len(fmt_char)}")
+        bo = self._get_byteorder_as_str()[1]
+        return ustruct.unpack(bo + fmt_char, source)
+
+    @micropython.native
+    def is_big_byteorder(self) -> bool:
+        return self.big_byte_order
 
     def get_id(self):
         raise NotImplementedError
--- sensor_pack/bus_service.py
+++ sensor_pack/bus_service.py
@@ -11,15 +11,25 @@ class BusAdapter:
     def __init__(self, bus):
         self.bus = bus
 
-    def read_register(self, device_addr: int, reg_addr: int, bytes_count: int = 2):
+    def read_register(self, device_addr: int, reg_addr: int, bytes_count: int) -> bytes:
         """считывает из регистра датчика значение.
-        bytes_count - размер значения в байтах"""
+        device_addr - адрес датчика на шине.
+        reg_addr - адрес регистра в адресном пространстве датчика.
+        bytes_count - размер значения в байтах.
+        reads value from sensor register.
+        device_addr - address of the sensor on the bus.
+        reg_addr - register address in the address space of the sensor"""
         raise NotImplementedError
 
     def write_register(self, device_addr: int, reg_addr: int, value: int,
-                       bytes_count: int = 2, byte_order: str = "big"):
+                       bytes_count: int, byte_order: str):
         """записывает данные value в датчик, по адресу reg_addr.
-        bytes_count - кол-во записываемых данных"""
+        bytes_count - кол-во записываемых байт из value.
+        byte_order - порядок расположения байт в записываемом значении.
+        writes value data to the sensor, at reg_addr.
+        bytes_count - number of bytes written from value.
+        byte_order - the order of bytes in the value being written.
+        """
         raise NotImplementedError
 
     def read(self, device_addr, n_bytes: int) -> bytes:
@@ -34,13 +44,13 @@ class I2cAdapter(BusAdapter):
         super().__init__(bus)
 
     def write_register(self, device_addr: int, reg_addr: int, value: int,
-                       bytes_count: int = 2, byte_order: str = "big"):
+                       bytes_count: int, byte_order: str):
         """записывает данные value в датчик, по адресу reg_addr.
         bytes_count - кол-во записываемых данных"""
         buf = value.to_bytes(bytes_count, byte_order)
         return self.bus.writeto_mem(device_addr, reg_addr, buf)
 
-    def read_register(self, device_addr: int, reg_addr: int, bytes_count: int = 2):
+    def read_register(self, device_addr: int, reg_addr: int, bytes_count: int) -> bytes:
         """считывает из регистра датчика значение.
         bytes_count - размер значения в байтах"""
         return self.bus.readfrom_mem(device_addr, reg_addr, bytes_count)
