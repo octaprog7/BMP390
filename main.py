37851b1d7a1df19a69aae508afecfb79a8b87fcc
roman <kolbasilyvasily@yandex.ru>
2026-05-02 22:23:32 +0500

    Небольшие правки.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# This is a sample Python script.
+from micropython import const
 from machine import I2C, Pin
 import bmp390mod
 import time
@@ -10,6 +10,13 @@ def pa_mmhg(value: float) -> float:
     Convert air pressure from Pa to mm Hg."""
     return 7.50062E-3 * value
 
+I2C_ID: int = const(1)
+SCL_PIN: int = const(7)
+SDA_PIN: int = const(6)
+I2C_FREQ: int = const(400_000)
+SENSOR_ADDR: int = const(0x77)
+ITERATIONS: int = const(33)
+
 
 if __name__ == '__main__':
     # пожалуйста установите выводы scl и sda в конструкторе I2C, для вашей платы, иначе ничего не заработает!
@@ -17,10 +24,10 @@ if __name__ == '__main__':
     # https://docs.micropython.org/en/latest/library/machine.I2C.html#machine-i2c
     # bus =  I2C(scl=Pin(4), sda=Pin(5), freq=100000)   # на esp8266    !
     # i2c = I2C(id=0, scl=Pin(13), sda=Pin(12), freq=400_000)  # on Arduino Nano RP2040 Connect
-    i2c = I2C(id=1, scl=Pin(7), sda=Pin(6), freq=400_000)   # on Raspberry Pi Pico
-    adaptor = I2cAdapter(i2c)
+    i2c = I2C(id=I2C_ID, scl=Pin(SCL_PIN), sda=Pin(SDA_PIN), freq=I2C_FREQ)   # on Raspberry Pi Pico
+    adapter = I2cAdapter(i2c)
     # ps - pressure sensor
-    ps = bmp390mod.Bmp390(adaptor)
+    ps = bmp390mod.Bmp390(adapter=adapter, address=SENSOR_ADDR)
     res = ps.get_id()
     print(f"chip_id: {res}")
     # если я не вызывал этот метод, то датчик не входил в режим
@@ -43,26 +50,26 @@ if __name__ == '__main__':
     print("Режим однократных измерений по запросу!")
     print(f"pwr mode: {ps.get_power_mode()}")
     print(f"время преобразования в [мкс]: {ps.get_conversion_cycle_time()}")
-    for _ in range(20):
+    for _ in range(ITERATIONS):
         ps.start_measurement(enable_press=True, enable_temp=True, mode=1)
         delay_func(300)
         temperature_ready, pressure_ready, cmd_ready = ps.get_data_status()
         if cmd_ready and pressure_ready:
             t, p = ps.get_temperature(), ps.get_pressure()
             pm = ps.get_power_mode()
-            print(f"Temperature: {t} \xB0C; pressure: {p} hPa ({pa_mmhg(p)} mm Hg); pwr_mode: {pm} ")
+            print(f"Temperature: {t} \xB0C; pressure: {p} Pa ({pa_mmhg(p)} mm Hg); pwr_mode: {pm} ")
         else:
             print(f"Data ready: temp {temperature_ready}, press {pressure_ready}")
     #
+    _min_p, _max_p = 1E6, 0
     print("Режим непрерывных периодических измерений!")
     ps.start_measurement(enable_press=True, enable_temp=True, mode=2)
     print(f"pwr mode: {ps.get_power_mode()}")
     for values in ps:
         delay_func(300)
-        d_status = ps.get_data_status()
-        if d_status.cmd_decoder_ready and d_status.press_ready:
-            t, p, tme = values.T, values.P, ps.get_sensor_time()
-            pm = ps.get_power_mode()
-            print(f"Temperature: {t} \xB0C; pressure: {p} hPa ({pa_mmhg(p)} mm Hg); pwr_mode: {pm}")
-        else:
-            print(f"Data ready: temp {d_status.temp_ready}, press {d_status.press_ready}")
+        t, p = values.T, values.P
+        tme = ps.get_sensor_time()
+        _min_p = min(_min_p, p)
+        _max_p = max(_max_p, p)
+        if t is not None and p is not None:  # достаточно проверки на None
+            print(f"T={t:.2f}°C, P={p:.2f} Pa, time={tme}, min_P={_min_p}, max_P={_max_p}")
